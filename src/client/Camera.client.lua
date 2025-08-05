351ce2161eab95e3d349bf7588f53d9f06ae9dc0
Precipitating <92101891+Precipitating@users.noreply.github.com>
2025-08-05 07:53:19 +0100

    + Added green click to walk when SafechatterHorror mode is fired + Revamped NPC logic: NO coroutines and state driven + NPC now tweens towards you when interacted with + Added camera detection code, that detects if a specified part is in view, enabled via remote + Weeping angel guest mode WIP

---
+++ src/client/Camera.client.lua
@@ -0,0 +1,38 @@
+local CollectionService = game:GetService("CollectionService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local CameraEnableRemote = ReplicatedStorage.Shared.Remotes.EnableCameraLookDetection
+local IsCameraLookingAtPartRemote = ReplicatedStorage.Shared.Remotes.IsCameraLookingAtPart
+local loopHandle
+local wasVisible = false
+local function IsModelVisible(part)
+	local camera = workspace.CurrentCamera
+
+	if part:IsA("BasePart") then
+		local _, onScreen = camera:WorldToViewportPoint(part.Position)
+		if onScreen then
+			return true
+		end
+	end
+
+	return false
+end
+
+local function StartLooking(partToFind)
+	loopHandle = RunService.RenderStepped:Connect(function()
+		local isVisible = IsModelVisible(partToFind)
+		if isVisible ~= wasVisible then
+			wasVisible = isVisible
+			IsCameraLookingAtPartRemote:FireServer(isVisible)
+		end
+	end)
+end
+
+CameraEnableRemote.OnClientEvent:Connect(function(enabled, partToFind)
+	if enabled then
+		StartLooking(partToFind)
+	else
+		loopHandle:Disconnect()
+		loopHandle = nil
+	end
+end)
